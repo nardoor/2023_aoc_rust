87f51f3a4aaf877019b99aa4a63855bbef2b5363
nardor <nard0r@protonmail.com>
2023-12-11 21:59:02 +0100

    Day 11 - part 1

---
+++ src/bin/11.rs
@@ -0,0 +1,183 @@
+use std::fmt::Debug;
+
+use itertools::Itertools;
+advent_of_code::solution!(11);
+
+#[derive(PartialEq, Eq, Clone)]
+enum Observable {
+    Galaxy,
+    Void,
+}
+
+impl Debug for Observable {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Observable::Galaxy => "#",
+            Observable::Void => ".",
+        })
+    }
+}
+
+impl From<char> for Observable {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Self::Void,
+            '#' => Self::Galaxy,
+            _ => panic!(),
+        }
+    }
+}
+
+impl Debug for Universe {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for line in &self.univ {
+            for o in line {
+                o.fmt(f)?
+            }
+            f.write_str("\n")?
+        }
+        Ok(())
+    }
+}
+
+struct Universe {
+    univ: Vec<Vec<Observable>>,
+}
+
+impl From<&str> for Universe {
+    fn from(value: &str) -> Self {
+        let universe = Universe {
+            univ: value
+                .lines()
+                .map(|l| l.chars().map(Observable::from).collect())
+                .collect(),
+        };
+        universe
+    }
+}
+
+impl Universe {
+    fn expand(&mut self) {
+        let lines_to_expand: Vec<usize> = self
+            .univ
+            .iter()
+            .enumerate()
+            .filter_map(|(line_index, line)| {
+                if line.iter().all(|o| *o == Observable::Void) {
+                    Some(line_index)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        let width = self.univ[0].len();
+        let columns_to_expand: Vec<usize> = (0..width)
+            .filter(|column_index| {
+                self.univ
+                    .iter()
+                    .all(|line| line[*column_index] == Observable::Void)
+            })
+            .collect();
+
+        let mut col_delta = 0;
+        for col in columns_to_expand {
+            self.univ
+                .iter_mut()
+                .for_each(|v| v.insert(col + col_delta, Observable::Void));
+            col_delta += 1;
+        }
+
+        let line_vec_template =
+            Vec::from_iter(std::iter::repeat(Observable::Void).take(self.univ[0].len()));
+
+        let mut line_delta = 0;
+        for line in lines_to_expand {
+            self.univ
+                .insert(line + line_delta, line_vec_template.clone());
+            line_delta += 1;
+        }
+    }
+
+    fn iter<'a>(&'a self) -> GalaxyIterator<'a> {
+        GalaxyIterator {
+            universe: self,
+            current_pos: (0, 0),
+            universe_dims: (self.univ[0].len(), self.univ.len()),
+        }
+    }
+}
+
+struct GalaxyIterator<'a> {
+    universe: &'a Universe,
+    current_pos: (usize, usize),
+    universe_dims: (usize, usize),
+}
+
+impl<'a> Iterator for GalaxyIterator<'a> {
+    type Item = (usize, usize);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_pos.0 + 1 >= self.universe_dims.0 {
+            self.current_pos.1 += 1;
+            self.current_pos.0 = 0;
+        } else {
+            self.current_pos.0 += 1;
+        }
+
+        // current line
+        if self.current_pos.0 != 0 {
+            for x in self.current_pos.0..self.universe_dims.0 {
+                if self.universe.univ[self.current_pos.1][x] == Observable::Galaxy {
+                    self.current_pos.0 = x;
+                    return Some(self.current_pos);
+                }
+            }
+            self.current_pos.1 += 1;
+        }
+        for y in self.current_pos.1..self.universe_dims.1 {
+            for x in 0..self.universe_dims.0 {
+                if self.universe.univ[y][x] == Observable::Galaxy {
+                    self.current_pos = (x, y);
+                    return Some(self.current_pos);
+                }
+            }
+        }
+        None
+    }
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let mut univ = Universe::from(input);
+    // println!("Before expand: \n{univ:?}");
+    univ.expand();
+    // println!("After expand: \n{univ:?}");
+    univ.iter()
+        .combinations(2)
+        .map(|combin| {
+            let g1 = combin[0];
+            let g2 = combin[1];
+            g2.1.abs_diff(g1.1) + g1.0.abs_diff(g2.0)
+        })
+        .reduce(|acc, el| acc + el)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(374));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
